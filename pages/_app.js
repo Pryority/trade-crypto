b4d6f2b561e767e17bd2926780fdf7fd84cbefc3
pryority <matthewapryor@gmail.com>
2022-11-04 22:17:57 -0400

    add Gnosis and Avalanche as available chains

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,31 @@
-import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
 import { 
   getDefaultWallets, 
   RainbowKitProvider 
-} from "@rainbow-me/rainbowkit";
+} from '@rainbow-me/rainbowkit';
 import { 
   chain, 
   configureChains, 
   createClient, 
   WagmiConfig 
-} from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { Gnosis, Avalanche } from '../components/CustomChains';
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli, chain.polygonMumbai],
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, Gnosis, Avalanche, chain.goerli, chain.polygonMumbai],
   [
     alchemyProvider({ alchemyId: process.env.ALCHEMY_ID}),
+    jsonRpcProvider({ rpc: () => ({ http: 'https://rpc.ankr.com/gnosis' }) }),
     publicProvider()
   ]
 );
 
 const { connectors } = getDefaultWallets({ 
-  appName: "Uniswap Crypto Trade Dapp",
+  appName: 'Uniswap Crypto Trade Dapp',
   chains
 });
 
