af14324673683448ce31c3ce2ec3f929c26de94c
pryority <matthewapryor@gmail.com>
2022-11-04 21:52:10 -0400

    initial commit: add RainbowKit, Wagmi, Ethers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { 
+  getDefaultWallets, 
+  RainbowKitProvider 
+} from "@rainbow-me/rainbowkit";
+import { 
+  chain, 
+  configureChains, 
+  createClient, 
+  WagmiConfig 
+} from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.goerli, chain.polygonMumbai],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID}),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({ 
+  appName: "Uniswap Crypto Trade Dapp",
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
