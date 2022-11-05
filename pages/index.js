013673671431cc627c5d27d064374750fad0dde5
pryority <matthewapryor@gmail.com>
2022-11-04 22:30:29 -0400

    add Uniswap SwapWidget

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { SwapWidget } from '@uniswap/widgets';
+import '@uniswap/widgets/fonts.css';
 
 export default function Home() {
   return (
     <div className='flex flex-col min-h-screen w-full items-center justify-center'>
-      <ConnectButton />
+      <div className='flex flex-col items-center space-y-4'>
+        <ConnectButton />
+        <SwapWidget />
+      </div>
     </div>
   );
 }
